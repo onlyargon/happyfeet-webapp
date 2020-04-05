7c7214aaa346efd3371d69415c448dd29e41e824
idsnext <aruna.wickramasinghe@idsnext.com>
2020-04-05 16:15:04 +0530

    update base url

--- src/app/helpers/appConstant.ts
+++ src/app/helpers/appConstant.ts
@@ -1,2 +1,2 @@
-export const HOME_URL = "https://happyfeet-web-api.herokuapp.com//";
+export const HOME_URL = "https://happyfeet-web-api.herokuapp.com/";
 // export const HOME_URL = "http://localhost:3000/";
